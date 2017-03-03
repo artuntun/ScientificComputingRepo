2bccf0ca9d32e42fe5122d8204d2ebaac9d389ea
manuwhs <manuwhs@gmail.com>
2017-03-03 10:41:36 +0100

    eh

---
+++ Assignment1/Manu's part/ImplicitEulers1.m
@@ -0,0 +1,50 @@
+function [tnList,ynList] = ExplicitEulers(func,tspan,N,Y0, Niter)
+%
+% This function solves a general first-order Initial Value Problem
+% of the form
+%                u? = f(u,t),  u(tstart) = eta
+%
+% using Euler?s Method in n steps (constant step size).
+%
+% INPUT:
+%    func  : a function handle to function f(u,t)
+%    tspan : a 1x2 array of the form [tstart tend]
+%    N     : total number of steps in tspan
+%    y0   : initialvalue(s)
+%    param : parameters to be passed to func
+%
+sizeY = size(Y0);
+Ndim = sizeY(1);
+sizeTspan = size(tspan);
+Ninit = sizeTspan(2);  % We can be given only the end time, then the begining is 0
+
+ynList = zeros(Ndim,N+1);
+tnList = zeros(Ndim,N+1);
+
+%% Initialization
+if (Ninit == 1)
+    tbegin = 0;
+    tend = tspan;
+elseif(Ninit == 2) % [tbegin tend]
+    tbegin = tspan(1,1);
+    tend = tspan(1,2);
+end
+dt = (tend - tbegin)/N;
+
+tnList(1) = tbegin;
+ynList(:,1) = Y0;
+
+%% Loop
+
+for k = 1:N
+    yn1Est = ynList(:,k);
+    for it = 1:Niter
+%         if (it == 1) || (it == Niter)
+%             yn1Est
+%         end
+        f = feval(func,tnList(k) + dt,yn1Est);
+        yn1Est = ynList(:,k)+dt*f;
+    end
+    ynList(:,k+1) = yn1Est;
+    tnList(k+1) = tnList(k)+dt;
+end
---
+++ Assignment1/Manu's part/newtonRootFinding.m
@@ -0,0 +1,20 @@
+function [ xn1 ] = newtonRootFinding(x0,tol)
+%UNTITLED20 Summary of this function goes here
+%   Detailed explanation goes here
+xn = x0;
+maxIt = 100;
+[f,fprime]=f1(x0);
+err = 1000;
+it = 0;
+
+while err>=tol && it < maxIt
+    it = it +1;
+    [f,fprime]=f1(xn);
+    xn1 = xn - (f/fprime);
+    [f,fprime]=f1(xn1);
+    err = f;
+    xn = xn1;
+end
+
+end
+
